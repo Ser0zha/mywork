5bd3b298599645dd0288f3ed6b08d6bfd6447be1
Ser0zha <137228126+Ser0zha@users.noreply.github.com>
2023-12-02 18:25:14 +0300

    Update A3.5.12.c

--- my_work/A3.5.12.c
+++ my_work/A3.5.12.c
@@ -1,59 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define N 101
-int input_string(char* s) {
-	char chr;
-	int i = 0;
-	printf("Enter your string: ");
-	scanf_s("%c", &chr);
-	while (1) {
-		while (('a' <= chr && chr <= 'z') || ('0' <= chr && chr <= '9') || (chr == (',', ';'))) {
-			s[i] = chr;
-			i++;
-			if (i > N - 1) {
-				printf("�ontent is full");
-				return 0;
-			}
-			scanf_s("%c", &chr);
-		}
-		if (chr == ' ' && i != 0) {
-			s[i] = ' ';
-			i++;
-			if (i > N - 1) {
-				printf("�ontent is full");
-				return 0;
-			}
-		}
-		while (chr == ' ')
-			scanf_s("%c", &chr);
-		if (chr == '.') {
-			s[i] = '\0';
-			break;
-		}
-	}
-	return 1;
+#include <stdlib.h>
+
+typedef struct Word {
+	char* inf; // Массив символов
+	size_t n_elements; // Сколько букв
+	struct Word* next;
+} Word;
+
+static Word* creat_word() {
+    Word* word = (Word*)malloc(sizeof(Word));
+    word->next = NULL;
+    word->n_elements = 0;
+    return word;
 }
-void quantityChar(char* s, int* k) {
-	int i;
-	for (i = 0; i < 26; i++) {
-		k[i] = 0;
-	}
-	for (i = 0; s[i] != '\0'; i++) {
-		k[s[i] - 'a']++;
-	}
+
+static int scan_word(Word* word, int *max) {
+    *max = 0;
+    char* str = (char*)malloc(word->n_elements + 1);
+    char c;
+    while (scanf("%c", &c) && (c != ' ' && c != ',')) {
+        if ('0' <= c && c <= '9') {
+            free(str);
+            word->n_elements = 0;
+            word->inf = 0;
+            *max = -1;
+            printf("Error");
+            return 1;
+        }
+        if ((c == '.') || (c == '!') || (c == '?')) {
+            str[word->n_elements] = 0;
+            word->inf = str;
+            return 1;
+        }
+        str[word->n_elements] = c;
+        word->n_elements++;
+        if (*max < word->n_elements)
+            *max = word->n_elements;
+        str = (char*)realloc(str, word->n_elements + 1);
+        if (word->n_elements > 20) {
+            free(str);
+            word->n_elements = 0;
+            word->inf = 0;
+            printf("Error");
+            return 1;
+        }
+    }
+    str[word->n_elements] = 0;
+    word->inf = str;
+    return 0;
 }
-void print(int* k) {
-	char ch;
-	for (ch = 'a'; ch <= 'z'; ch++)
-		if (k[ch - 'a'])
-			printf("%c -> %d\n", ch, k[ch - 'a']);
+int scan_text(Word* word) {
+    int maxi = 0, s = 1;
+    Word* pWord = word;
+    while (!scan_word(pWord, &maxi)) {
+        s++;
+        if (s > 30) {
+            printf("Error");
+            return -1;
+        }
+        pWord->next = creat_word();
+        pWord = pWord->next;
+    }
+    return maxi;
+}
+void print_word_index(Word *word, int maxx) {
+    Word *pList = word;
+    short n = 0, f = 0;
+    while (pList!=NULL) {
+        if (pList->n_elements == maxx) {
+            printf("%s ", pList->inf);
+        }
+        pList=pList->next;
+        f++;
+    }
+    if (f = 0)
+        printf("Not found");
 }
 void main() {
-	char str[N];
-	int kol[26];
-	if (!(input_string(str))) {
-		printf("Error");
-		return;
-	}
-	quantityChar(str, kol);
-	print(kol);
+    int max;
+	Word* word_list = creat_word();
+	max = scan_text(word_list);
+    if (max == -1)
+        return 0;
+	print_word_index(word_list, max);
 }
