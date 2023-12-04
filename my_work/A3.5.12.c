9a1c4914b6fd0e7f500fcf60a3514c02705c4cd6
Ser0zha <137228126+Ser0zha@users.noreply.github.com>
2023-12-04 18:43:03 +0300

    Update A3.5.12.c

--- my_work/A3.5.12.c
+++ my_work/A3.5.12.c
@@ -1,31 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#define charlimit 20
+#define strlimit 30
 
 typedef struct Word {
-	char* inf; // Массив символов
-	size_t n_elements; // Сколько букв
-	struct Word* next;
+    char* inf; // Массив символов
+    size_t n_elements; // Сколько букв
+    struct Word* next;
 } Word;
 
 static Word* creat_word() {
-    Word* word = (Word*)malloc(sizeof(Word));
+    Word* word;
+    if (!(word = (Word*)malloc(sizeof(Word)))) {
+        printf("Error, memory!");
+        return NULL;
+    }
     word->next = NULL;
     word->n_elements = 0;
     return word;
 }
 
-static int scan_word(Word* word, int *max) {
+static int scan_word(Word* word, int* max, int *ss) {
     *max = 0;
-    char* str = (char*)malloc(word->n_elements + 1);
+    char* str;
     char c;
-    while (scanf("%c", &c) && (c != ' ' && c != ',')) {
+    if (!(str = (char*)malloc(word->n_elements + 1))) {
+        printf("Error, memory!");
+        return NULL;
+    } 
+    while (scanf("%c", &c) && (c != ' ')) {
         if ('0' <= c && c <= '9') {
             free(str);
             word->n_elements = 0;
             word->inf = 0;
             *max = -1;
-            printf("Error, use number!");
+            printf("Error, u can`t use number!");
             return 1;
         }
         if ((c == '.') || (c == '!') || (c == '?')) {
@@ -33,12 +43,20 @@ static int scan_word(Word* word, int *max) {
             word->inf = str;
             return 1;
         }
+        if (!(('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))) {
+            printf("U cannot use prohibited characters!");
+            return 1;
+        }
         str[word->n_elements] = c;
         word->n_elements++;
         if (*max < word->n_elements)
             *max = word->n_elements;
-        str = (char*)realloc(str, word->n_elements + 1);
-        if (word->n_elements > 20) {
+        ss++;
+        if (!(str = (char*)realloc(str, word->n_elements + 1))) {
+            printf("Error, memory!");
+            return NULL;
+        }
+        if (word->n_elements > charlimit) {
             free(str);
             word->n_elements = 0;
             word->inf = 0;
@@ -53,10 +71,9 @@ static int scan_word(Word* word, int *max) {
 int scan_text(Word* word) {
     int maxi = 0, s = 1;
     Word* pWord = word;
-    while (!scan_word(pWord, &maxi)) {
-        s++;
-        if (s > 30) {
-            printf("Error, sentence is full");
+    while (!scan_word(pWord, &maxi, &s)) {
+        if (s > strlimit) {
+            printf("Error, sentence is full!");
             return -1;
         }
         pWord->next = creat_word();
@@ -64,21 +81,37 @@ int scan_text(Word* word) {
     }
     return maxi;
 }
-void print_word_index(Word *word, int maxx) {
-    Word *pList = word;
+void free_word(Word* first_word) {
+    Word* pList = first_word, * freeList;
+    while (pList != NULL) {
+        free(pList->inf);
+        freeList = pList;
+        pList = pList->next;
+        free(freeList);
+    }
+}
+void print_word_index(Word* word, int maxx) {
+    Word* pList = word;
     short n = 0;
-    while (pList!=NULL) {
+    while (pList != NULL) {
         if (pList->n_elements == maxx) {
             printf("%s ", pList->inf);
         }
-        pList=pList->next;
+        pList = pList->next;
     }
 }
 void main() {
-	int max;
-	Word* word_list = creat_word();
-	max = scan_text(word_list);
-    	if (max == -1)
-        	return 0;
-	print_word_index(word_list, max);
+    int max;
+    printf("Enter sentence: ");
+    Word* word_list = creat_word();
+    max = scan_text(word_list);
+    if (max == -1)
+        return;
+    else if (max == 0) {
+        printf("NULL");
+        return;
+    }
+    printf("Max words: ");
+    print_word_index(word_list, max);
+    free_word(word_list);
 }
